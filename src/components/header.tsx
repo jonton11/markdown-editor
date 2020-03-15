0fc8147a45e858bf0dc2fe94811481847a90f56e
Jonathan Wong <jon.wong92@gmail.com>
2020-03-14 17:52:12 -0700

    Style header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,26 +5,43 @@ import { FaMoon } from 'react-icons/fa';
 
 import { css, jsx } from '@emotion/core';
 
-function header () {
-    const theme = 'night';
+interface Props {
+    toggleTheme: () => void,
+    theme: string
+}
 
+const darkCss = `
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    background-color: #eee;
+    padding: 32px;
+`;
+
+const lightCss = `
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    background-color: #f8f541;
+    padding: 24px 32px;
+    box-shadow: 0px -2px 8px #000;
+    font-size: 16px;
+`;
+
+
+const Header: React.FC<Props> = ({ theme, toggleTheme }) => {
     return (
-        <header
-        css={css`
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        background-color: #eee;
-        padding: 32px;
-        `}>
+        <header css={ css(( theme === 'dark' ) ? `${darkCss}` : `${lightCss}`) }>
             <div className="header-title">
                 Markdown Editor v1
             </div>
-            <div>
+            <div css={css`cursor: pointer`}
+                onClick={ toggleTheme }>
+
                 { theme === 'night' ? <FaMoon/> : <FiSun/> }
             </div>
         </header>
     )
 }
 
-export default header;
+export default Header;
