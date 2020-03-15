11d93ca9c9535956c219c5064bb036399ff0bf16
Jonathan Wong <jon.wong92@gmail.com>
2020-03-14 17:58:14 -0700

    Add main component

---
+++ src/components/main.tsx
@@ -0,0 +1,29 @@
+
+import React, { useState } from 'react';
+
+import { css, jsx } from '@emotion/core';
+import { RowFlex } from './shared';
+import Editor from './editor';
+import Preview from './preview';
+
+interface Props {
+    theme: string
+}
+
+const Main: React.FC<Props> = ({ theme }) => {
+    const [ markdownContent, setMarkdownContent ] = useState<string>(``);
+
+    return (
+        <RowFlex
+        css={css`
+            padding: 32px;
+            padding-top: 0px;
+            height: calc(100vh - 170px);
+            `}>
+      <Editor theme={theme} markdownContent={markdownContent} setMarkdownContent={setMarkdownContent}/>
+      <Preview theme={theme} markdownContent={markdownContent}/>
+    </RowFlex>
+    )
+}
+
+export default Main;
